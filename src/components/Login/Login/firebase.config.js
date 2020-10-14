45023411ad3328f2f787549904c312d065c18998
Misk007 <mdrahmanmiskatur@gmail.com>
2020-10-14 22:08:49 +0600

    client done

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDLWqbUPoBqkraKymcc8-NeZyXnFLWP5sw",
+  authDomain: "doctors-chamber-by-misk.firebaseapp.com",
+  databaseURL: "https://doctors-chamber-by-misk.firebaseio.com",
+  projectId: "doctors-chamber-by-misk",
+  storageBucket: "doctors-chamber-by-misk.appspot.com",
+  messagingSenderId: "347464375206",
+  appId: "1:347464375206:web:9047e48cff5075ae140d67"
+};
+export default firebaseConfig;
